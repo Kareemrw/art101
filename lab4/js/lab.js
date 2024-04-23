339bc98445ef8accce12b2f2750daed1687c7347
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-04-23 15:45:00 -0700

    update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -3,15 +3,17 @@
 // Kareem Wellington <krwellin@ucsc.edu>
 // April 2024
 
-//Loop to checking if win condition is met 
-  //Print out each option: Rock, Paper, Scissors
-  //Allow the user to select Rock, Paper, or Scissors
-  //Have computer randomly select Rock, Paper, or Scissors
-  //If User and Computer selected the same thing
-    //Restart Game
-  //If they are different, choose the winner
-    //User selects rock and computer selects scissors or User selects scissors and computer selects paper or User selects paper and computer selects rock
-      //User wins
-  //If none of those conitions are met
-    //Computer wins
+//Loop to check if win condition is met 
+	//Print out each option: Rock, Paper, Scissors
+	//Allow the user to select Rock, Paper, or Scissors
+	//Print out the user's choice
+	//Have computer randomly select Rock, Paper, or Scissors
+	//Print out what the computer choice
+	//If User and Computer selected the same thing
+		//Restart Game
+	//If they are different, choose the winner
+	//User selects rock and computer selects scissors or User selects scissors and computer selects paper or User selects paper and computer selects rock
+		//User wins
+	//If none of those conitions are met
+		//Computer wins
     
