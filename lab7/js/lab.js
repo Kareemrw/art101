6f3ece90b3dec4834582dfef8e12927bf295d55e
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-05-02 14:53:42 -0700

    updating lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,27 +1,20 @@
-// lab.js - Testing objects and arrays in javascript
+// lab.js - Creating a function that sorts a name 
 // Author: Kareem Wellington <krwellin@ucsc.edu>
-// Date: 4/23/2024
+// Date: 5/2/2024
 
-//Define Variables
-
-const myTransport = ['foot', 'car', 'bus'];
-
-//Creating an object
-
-const myMainRide = {
-  make: "Lexus",
-  model: "CT200h",
-  color: "silver",
-  year: 2012,
-  
-  age: function(){
-    return 2024 - this.year;
-  }
+// user input
+let userName = window.prompt("Please enter your name so I can sort it.");
+document.getElementById("username").innerHTML = userName;
+// sortUserName - takes user input and sorts letters of the name
+function sortUserName(name){
+  // splits the username into an array of lowercase strings 
+  let nameWordsLower = name.toLowerCase().split(' ');
+  //splits each string in the array into an array of characters, 
+  //sorts the characters and rejoins them into strings
+  let wordsSorted = nameWordsLower.map(i => i.split('').sort().join(''));
+  // Turns the array of strings into one string maintaining spaces
+  let sortedName = wordsSorted.join(' ');
+  return sortedName;
 }
-
-//output
-document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
-
-//writing the object
-document.writeln("My Main Ride: <pre>", 
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+// output
+document.getElementById("sortedname").innerHTML = "Here's your sorted name: " + sortUserName(userName);
