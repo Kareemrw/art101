5d7b3b5d11ca0301538a7e06464fbd817add96e0
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-05-02 13:39:00 -0700

    adding lab 7 files

---
+++ lab7/js/lab.js
@@ -0,0 +1,27 @@
+// lab.js - Testing objects and arrays in javascript
+// Author: Kareem Wellington <krwellin@ucsc.edu>
+// Date: 4/23/2024
+
+//Define Variables
+
+const myTransport = ['foot', 'car', 'bus'];
+
+//Creating an object
+
+const myMainRide = {
+  make: "Lexus",
+  model: "CT200h",
+  color: "silver",
+  year: 2012,
+  
+  age: function(){
+    return 2024 - this.year;
+  }
+}
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+//writing the object
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
