5367f2cffaab0d0affe7230e8bb0cd3be4a6e873
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-05-30 22:58:22 -0700

    lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,9 @@
+// lab.js - Creating a Fizz Buzz Boom function
+// Author: Kareem Wellington <krwellin@ucsc.edu>
+// Date: 5/26/2024
+
+let numArray =  [1,2,3,-4,-1,4];
+numArray.sort(function(a,b) {
+  return a - b;
+});
+console.log(numArray);
